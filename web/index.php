06e3289f8dbb4ed32f35d0a73c3478d1997d64ab
Thierry Delbart <thierry@thde.pro>
2016-06-09 10:27:56 -0300

    New test for Mongo Connection

--- web/index.php
+++ web/index.php
@@ -1,12 +1,13 @@
 <?php
 
-echo "hello";
+echo "hello<br><br>";
 
 // connexion
 $m = new MongoClient("mongodb://heroku_pwk905m9:6pqscbe6c39kogeq3861rilvji@ds011314.mlab.com:11314/heroku_pwk905m9");
+//$m = new MongoClient();
 
 // sélection d'une base de données
-$db = $m->comedy;
+$db = $m->heroku_pwk905m9;
 
 // sélectionne une collection (analogue à une table de base de données relationnelle)
 $collection = $db->cartoons;
@@ -24,7 +25,7 @@ $cursor = $collection->find();
 
 // traverse les résultats
 foreach ($cursor as $document) {
-    echo $document["title"] . "\n";
+    echo $document["title"] . "<br>";
 }
 
 ?>
