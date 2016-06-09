332273bed3d9716328a15b2acb331002613453ae
Thierry Delbart <thierry@thde.pro>
2016-06-09 11:11:22 -0300

    Adding ENVIRONMENT check + deleting all docs on start

--- web/index.php
+++ web/index.php
@@ -1,29 +1,37 @@
 <?php
 
-echo "hello<br><br>";
-
 // connexion
-$m = new MongoClient("mongodb://heroku_pwk905m9:6pqscbe6c39kogeq3861rilvji@ds011314.mlab.com:11314/heroku_pwk905m9");
-//$m = new MongoClient();
+switch (getenv('ENVIRONMENT')) {
+	case 'production':
+		$m = new MongoClient(getenv('MONGODB_URI'));
+		break;
+	
+	default: // Mongo runs on localhost
+		$m = new MongoClient();
+		break;
+}
 
-// sélection d'une base de données
+// database select
 $db = $m->heroku_pwk905m9;
 
-// sélectionne une collection (analogue à une table de base de données relationnelle)
+// collection select (collection = table)
 $collection = $db->cartoons;
 
-// ajoute un enregistrement
+// Delete all documents from collection to start over (document = row)
+$collection->remove();
+
+// add one document
 $document = array( "title" => "Calvin and Hobbes", "author" => "Bill Watterson" );
 $collection->insert($document);
 
-// ajoute un autre enregistrement, avec une façon différente d'insertion
+// add another document
 $document = array( "title" => "XKCD", "online" => true );
 $collection->insert($document);
 
-// récupère tout de la collection
+// get all documents from collection
 $cursor = $collection->find();
 
-// traverse les résultats
+// loop all documents
 foreach ($cursor as $document) {
     echo $document["title"] . "<br>";
 }
