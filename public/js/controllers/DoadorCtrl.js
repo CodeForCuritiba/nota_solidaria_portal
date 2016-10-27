ed2ca8a8c4bc5c84ef0b77c3fdcd26d63e90bc15
Lucas Romano <lucas@lucas.home>
2016-10-27 11:58:20 -0400

    add search bar #2 and fixing icons size
    Signed-off-by: Lucas Romano <lucas@lucas.home>

--- public/js/controllers/DoadorCtrl.js
+++ public/js/controllers/DoadorCtrl.js
@@ -1,8 +1,9 @@
 // public/js/controllers/DoadorCtrl.js
 angular.module('DoadorCtrl', ['ngclipboard']).controller('DoadorController', ['$scope', 'Doador', '$sce', function($scope,Doador,$sce) {
 
-
     $scope.tagline = 'É fácil amar os que estão longe.<br/>Mas nem sempre é fácil amar os que vivem ao nosso lado.';
+    $scope.date1 = "";
+    $scope.date2 = "";
 
     Doador.get().then(function(response) {
 
@@ -46,6 +47,58 @@ angular.module('DoadorCtrl', ['ngclipboard']).controller('DoadorController', ['$
                 numberFixedLen(date.getHours(),2) + "h" + numberFixedLen(date.getMinutes(),2);
     }
 
+    $('#date1-calendar').datetimepicker({
+        format: 'D/MM/YYYY'
+    });
+    $('#date2-calendar').datetimepicker({
+        format: 'D/MM/YYYY'
+    });
+    $scope.date1 = "";
+    $("#date1-calendar").on("dp.change", function() { //have to do this cause ng-model does not bind with bootstrap-calendar
+        $scope.date1 = $("#date1").val();
+    });
+    $scope.date2 = "";
+    $("#date2-calendar").on("dp.change", function() {
+        $scope.date2 = $("#date2").val();
+    });
+
+    $scope.findNotasByDate = function() { //no api calls
+        var date1, date2, values, res;
+        if($scope.date1 == "") {
+            date1 = new Date("01 01 1970"); //get first date
+        } else {
+            // have to change from DD/MM/YYYY to MM/DD/YYYY to create Date object
+            values = $scope.date1.split("/");
+            res = values[1] + " " + values[0] + " " + values[2];
+            date1 = new Date(res);
+            console.log(res);
+        }
+
+        if($scope.date2 == "") {
+            date2 = new Date(); //get current date
+        } else {
+            values = $scope.date2.split("/");
+            res = values[1] + " " + values[0] + " " + values[2];
+            date2 = new Date(res);
+            console.log(res);
+        }
+
+        console.log(date1);
+        console.log(date2);
+        $scope.notas = [];
+
+       $scope.doadores.forEach(function(doador,key) {
+            if (doador.notas) {
+                for(var id in doador.notas) {
+                    if(doador.notas[id].donated_at > date1.getTime() && doador.notas[id].donated_at < date2.getTime()) {
+                        doador.notas[id].doador = doador;
+                        $scope.notas.push(doador.notas[id]);
+                    }
+                }
+            }
+        });
+    }
+
     $scope.setNotaExportado = function(doador_id, nota_index) {
         $scope.notas[nota_index].exportado = !$scope.notas[nota_index].exportado;
         return Doador.setNotaExportado(doador_id, nota_index);
@@ -78,4 +131,3 @@ angular.module('DoadorCtrl', ['ngclipboard']).controller('DoadorController', ['$
         return num;
     };
 });
-
